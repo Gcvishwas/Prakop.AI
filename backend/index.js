de44df0ad3d0893bec73d718a695bb81c2cb2763
Gcvishwas <gcvishwas17@gmail.com>
2025-12-03 13:48:39 +0545

    Add DB

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
+    credentials: true,
   })
 );
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to DB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 app.listen(port, () => {
+  connect();
   console.log("Server running on port 3000");
 });
