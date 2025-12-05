87306e29675c2c5defd044b68f802dd710dd3147
Gcvishwas <gcvishwas17@gmail.com>
2025-12-05 14:11:06 +0545

    Create ChatList to fetch title and chats according to user logged in

---
+++ frontend/src/Components/chatList/ChatList.jsx
@@ -0,0 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router";
+import Loader from "../Loader";
+
+const ChatList = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["userChats"],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/userChats`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
+  return (
+    <div className="flex flex-col h-full">
+      <span className="title font-semibold text-[10px] mb-2.5">DASHBOARD</span>
+      <Link to="/dashboard" className="p-2.5 rounded-[10px] hover:bg-[#2c2937]">
+        Create a new Chat
+      </Link>
+      <Link to="/" className="p-2.5 rounded-[10px] hover:bg-[#2c2937]">
+        Explore
+      </Link>
+      <Link to="/" className="p-2.5 rounded-[10px] hover:bg-[#2c2937]">
+        Contact
+      </Link>
+      <hr className="border-none h-[2px] bg-[#ddd] opacity-10 rounded-[5px] my-5" />
+      {/* Recent Chats */}
+      <span className="title font-semibold text-[10px] mb-2.5">
+        RECENT CHATS
+      </span>
+      <div className="flex flex-col overflow-y-auto scrollbar-hide">
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          "Something went wrong"
+        ) : (
+          data?.map((chat) => (
+            <Link
+              to={`dashboard/chats/${chat._id}`}
+              className="p-1.5 rounded-[10px] hover:bg-[#2c2937]"
+              key={chat._id}
+            >
+              {chat.title}
+            </Link>
+          ))
+        )}
+      </div>
+      <hr className="border-none h-[2px] bg-[#ddd] opacity-10 rounded-[5px] my-5" />
+      <div className="upgrade mt-auto flex items-center gap-2 text-[12px]">
+        <img src="/vite.svg" alt="logo" className="w-6 h-6" />
+        <div className="texts flex flex-col">
+          <span className="font-semibold">Upgrade </span>
+          <span className="text-[#888]">New Features coming soon!!!</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatList;
