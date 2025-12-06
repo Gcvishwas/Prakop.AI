94dddc3497f2a32ecec794d47a923eccf935430f
Gcvishwas <gcvishwas17@gmail.com>
2025-12-06 20:30:50 +0545

    Refactor ChatPage component to improve chat ID extraction and enhance loading/error handling

--- frontend/src/routes/Chat/ChatPage.jsx
+++ frontend/src/routes/Chat/ChatPage.jsx
@@ -5,7 +5,8 @@ import Markdown from "react-markdown";
 import Loader from "../../Components/Loader";
 
 const ChatPage = () => {
-  const chatId = useLocation().pathname.split("/").pop();
+  const path = useLocation().pathname;
+  const chatId = path.split("/").pop();
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["chat", chatId],
@@ -18,28 +19,24 @@ const ChatPage = () => {
   return (
     <div className="h-full flex flex-col items-center relative">
       <div className="flex-1 overflow-y-auto w-full flex justify-center">
-        <div className="w-1/2 flex flex-col gap-5 pb-32">
-          {isLoading && <Loader />}
-          {error && <div className="text-red-500 p-4">No Chats Found</div>}
-
-          {/* Render full history */}
-          {!isLoading &&
-            !error &&
+        <div className="w-1/2 flex flex-col gap-5">
+          {isLoading ? (
+            <Loader />
+          ) : error ? (
+            "Can't fetch chat"
+          ) : (
             data?.history?.map((message, i) => (
               <div
                 key={i}
-                className={
-                  message.role === "user"
-                    ? "bg-[#2c2937] rounded-2xl max-w-[80%] self-end p-5"
-                    : "bg-[#2c2937] rounded-2xl max-w-[80%] self-start p-5"
-                }
+                className={`bg-[#3a3a3a] rounded-2xl max-w-[80%] p-4 mt-2 ${
+                  message.role === "user" ? "self-end" : "self-start"
+                }`}
               >
                 <Markdown>{message.parts[0].text}</Markdown>
               </div>
-            ))}
-
-          {/* Message input + streaming */}
-          {!isLoading && !error && <NewPrompt data={data} />}
+            ))
+          )}
+          {data && <NewPrompt data={data} />}
         </div>
       </div>
     </div>
