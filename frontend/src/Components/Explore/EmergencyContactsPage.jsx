82d399bfedde3e761e863d7bdf45ea67901dce9f
Gcvishwas <gcvishwas17@gmail.com>
2025-12-08 22:12:15 +0545

    Create emergency contacts page

--- frontend/src/Components/Explore/EmergencyContactsPage.jsx
+++ frontend/src/Components/Explore/EmergencyContactsPage.jsx
@@ -1,7 +1,122 @@
-import React from "react";
-
+import { Phone, MapPin, Search } from "lucide-react";
+import { useEffect, useState } from "react";
+import Loader from "../Loader";
 const EmergencyContactsPage = () => {
-  return <div>Emergency</div>;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [contact, setContact] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredContacts, setFilteredContacts] = useState([]);
+  // fetching contacts
+  useEffect(() => {
+    const fetchContact = async () => {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/api/contacts`);
+        if (!res.ok) {
+          throw new Error("API request failed");
+        }
+        const data = await res.json();
+        setContact(data);
+      } catch (err) {
+        console.log(err);
+        setError("डेटा लोड गर्न असफल भयो !!!");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchContact();
+  }, []);
+
+  // filtering contacts
+  useEffect(() => {
+    if (!searchQuery) {
+      setFilteredContacts(contact);
+    } else {
+      const query = searchQuery.toLowerCase();
+      const filtered = contact.filter(
+        (c) =>
+          c.name?.toLowerCase().includes(query) ||
+          c.address?.toLowerCase().includes(query) ||
+          c.type?.toLowerCase().includes(query)
+      );
+      setFilteredContacts(filtered);
+    }
+  }, [searchQuery, contact]);
+  if (loading) {
+    return (
+      <div className="min-h-screen items-center justify-center flex bg-slate-900">
+        <div>
+          <Loader />
+          <p>लोड हुँदैछ..</p>
+        </div>
+      </div>
+    );
+  }
+  if (error)
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-slate-900 text-red-500">
+        <p>{error}</p>
+      </div>
+    );
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-slate-900 p-4 sm:p-8">
+      {/* Heading */}
+      <h1 className="text-2xl sm:text-3xl font-bold text-white mb-6 p-1">
+        आकस्मिक सम्पर्कहरू
+      </h1>
+      {/* Search Bar */}
+      <div className="mb-6">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="नाम, ठेगाना वा सेवा प्रकार खोज्नुहोस्..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full bg-slate-800 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <Search className="absolute w-5 h-5 right-4 top-1/2 transform -translate-y-1/2" />
+        </div>
+      </div>
+      {/* Contact List */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        {filteredContacts.length === 0 ? (
+          <div className="col-span-full text-center text-gray-400 py-10">
+            कुनै सम्पर्क फेला परेन
+          </div>
+        ) : (
+          filteredContacts.map((contact, index) => (
+            <div
+              key={index}
+              className="bg-white bg-opacity-10 rounded-xl p-4 border-white border border-opacity-10 hover:scale-105 transform transition backdrop-blur-md"
+            >
+              {/* Contact Name */}
+              <h1 className="text-lg font-semibold text-white mb-2 truncate">
+                {contact.name}
+              </h1>
+
+              {/* Contact Address */}
+              {contact.district !== "-" && (
+                <div className="flex items-center text-gray-300 text-sm mb-1">
+                  <MapPin className="w-4 h-4 mr-1" />
+                  <span>{contact.district}</span>
+                </div>
+              )}
+
+              {/* Contact Number */}
+              {contact.number && (
+                <div className="flex items-center text-gray-300 text-sm mb-1">
+                  <Phone className="w-4 h-4 mr-1" />
+                  <a href={`tel:${contact.number}`} className="hover:underline">
+                    {contact.number}
+                  </a>
+                </div>
+              )}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default EmergencyContactsPage;
