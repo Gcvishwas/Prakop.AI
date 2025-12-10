aa9463908b9854cad59587a6a092905e28e1fc1d
Gcvishwas <gcvishwas17@gmail.com>
2025-12-10 22:40:11 +0545

    Add animations

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         rotateOrbital: "rotateOrbital 200s linear infinite",
         ambulanceAnimate: "botAnimate 2s linear infinite alternate",
         slideBg: "slideBg 8s ease-in-out infinite alternate",
+        slideDown: "slideDown 0.6s ease-out",
+        slideIn: "slideIn 0.3s ease-out forwards",
       },
       keyframes: {
         rotateOrbital: {
@@ -20,6 +22,20 @@ module.exports = {
         slideBg: {
           "0%": { transform: "translateX(0)" },
           "100%": { transform: "translateX(-50%)" },
+        },
+        slideDown: {
+          "0%": { transform: "translateY(-20px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        slideIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
         },
       },
     },
